c91037f577a00d9c02abc7738a5540c6cd802881
whoopsh <434313466@qq.com>
2017-10-02 00:38:09 -0400

    update

--- main.py
+++ main.py
@@ -1,57 +1,46 @@
 # -*- coding: utf-8 -*-
 import sys;
 from bs4 import BeautifulSoup
-#from config import *;
 from googleapiclient.discovery import build
 from wordExpansion import *
 
 
 def main():
-	# Build a service object for interacting with the API. Visit
-	# the Google APIs Console <http://code.google.com/apis/console>
-	# to get an API key for your own application.
-#	client_key = "AIzaSyAPbX4JVlc8waFre4Zve1v8zx1VSfhijIk";
-#	engine_key = '008083549322187859573:yubw3z65huy';
-#	query = 'per se';
-#	precision = '0.9';
-
+	# Present input parameters.
 	client_key = sys.argv[1]
 	engine_key = sys.argv[2]
 	precision = sys.argv[3]
 	query = ' '.join(sys.argv[4:])
-	print query
-
 
 	service = build("customsearch", "v1",
 			developerKey=client_key)
 
-	print('Parameters:')
-	print('Client key = ' + client_key)
-	print('Engine key = ' + engine_key)
-	print('Query      = ' + query)
-	print('Precision  = ' + precision)
-	print('Google Search Results:')
-	print('=======================')
-
-	with open('transcript', 'a') as file:
-		file.write('Parameters:\n')
-		file.write('Client key = ' + client_key + '\n')
-		file.write('Engine key = ' + engine_key + '\n')
-		file.write('Query      = ' + query + '\n')
-		file.write('Precision  = ' + precision + '\n')
-		file.write('=======================\n')
-
-	#rele_doc = []
-	#irre_doc = []
-	
-	
+	# Present top-10 results and mark relevance in each iteration.
+	# Call expandWords implemented by Rocchio Algorithm to expand query.
+	# Terminate if precision >= target value or precision is zero.
 	while True:
+		print('Parameters:')
+		print('Client key = ' + client_key)
+		print('Engine key = ' + engine_key)
+		print('Query      = ' + query)
+		print('Precision  = ' + precision)
+		print('Google Search Results:')
+		print('=======================')
+
+		with open('transcript.txt', 'a') as file:
+			file.write('Parameters:\n')
+			file.write('Client key = ' + client_key + '\n')
+			file.write('Engine key = ' + engine_key + '\n')
+			file.write('Query      = ' + query + '\n')
+			file.write('Precision  = ' + precision + '\n')
+			file.write('=======================\n')
+
 		res = service.cse().list(
-			q=query,
-			cx=engine_key,
+			q = query,
+			cx = engine_key,
 		).execute()
 
-		doc =[]
+		doc = []
 		relevant = []
 		for i,item in enumerate(res['items']):
 			print 'Result '+str(i+1)
@@ -65,30 +54,31 @@ def main():
 			while tmp != 'y' and tmp != 'n' and tmp != 'N' and tmp != 'Y':
 				print 'Invalid input, please enter again.'
 				tmp = raw_input('Relevant (Y/N):?')
+
+			print ''
+			with open('transcript.txt', 'a') as file:
+				file.write('Result ' + str(i + 1) + '\n')
+				file.write('[\n')
+				file.write(' URL: ' + item['formattedUrl'].encode('utf-8') + '\n')
+				file.write(' Title: ' + BeautifulSoup(item['htmlTitle'], "html.parser").text.encode('utf-8') + '\n')
+				file.write(
+					' Description: ' + BeautifulSoup(item['htmlSnippet'], "html.parser").text.encode('utf-8') + '\n')
+				file.write(']' + '\n')
+				file.write('Relevant (Y/N):? ' + tmp.lower() + '\n')
+				file.write('\n')
+
 			if tmp.lower() == 'y':
 				relevant.append(1)
 			else:
 				relevant.append(0)
-			#if tmp.lower() == 'y':
-			# rele_doc.append(BeautifulSoup(item['htmlSnippet'], "html.parser").text)
-			#else:
 			doc.append(BeautifulSoup(item['htmlSnippet'], "html.parser").text)
-			print ''
-			with open('transcript', 'a') as file:
-				file.write('Result '+str(i+1)+'\n')
-				file.write('[\n')
-				file.write(' URL: '+item['formattedUrl'].encode('utf-8')+'\n')
-				file.write(' Title: '+BeautifulSoup(item['htmlTitle'], "html.parser").text.encode('utf-8')+'\n')
-				file.write(' Description: '+BeautifulSoup(item['htmlSnippet'], "html.parser").text.encode('utf-8')+'\n')
-				file.write(']'+'\n')
-				file.write('Relevant (Y/N):? '+ tmp.lower()+'\n')
-				file.write('\n')
+
 			count = 0
 			for i in relevant:
-				if i == 1: count+=1
-			count = count/10.0
+				if i == 1: count += 1
+			count = count / 10.0
 
-		query = expandWords(doc,relevant,query,count,float(precision))
+		query = expandWords(doc, relevant, query, count, float(precision))
 		if count >= float(precision) or count == 0:
 			return 
 
--- wordExpansion.py
+++ wordExpansion.py
@@ -5,22 +5,24 @@ from operator import add,sub
 with open("stopwords.txt", "r") as f:
     stop = f.read().splitlines() 
 alpha = 1
-beta = 0.5
-gamma = 0.5
-def expandWords (doc,relevant,query,prec,target_prec):
+beta = 0.75
+gamma = 0.15
+def expandWords (doc, relevant, query, prec, target_prec):
 	query_list = query.split(" ")
 	for word in query_list:
 		if word in stop:
 			stop.remove(word)
 	num = len(query_list)
 	R = sum(relevant)
-	NR = len(relevant)-R
+	NR = len(relevant) - R
+
+	# Vectorize results using tf-idf term weighting.
+	# Initialize query vector and update it based on relevant and irrelevant doc.
 	vectorizer = CountVectorizer(stop_words = stop)
 	corpus = doc
 	X = vectorizer.fit_transform(corpus)
-#	print vectorizer.get_feature_names()
 	res = TfidfTransformer(smooth_idf=False).fit_transform(X).toarray()
-#	print res
+
 	q0 =[0]*len(vectorizer.get_feature_names())
 	for word in query:
 		if word in vectorizer.get_feature_names():
@@ -29,12 +31,15 @@ def expandWords (doc,relevant,query,prec,target_prec):
 		if relevant[i] == 1:
 			temp = row
 			temp *= beta / R
-			q0 = map(add,q0,temp)
+			q0 = map(add, q0, temp)
 		elif relevant[i] == 0:
 			temp = row
 			temp *= gamma /NR
-			q0 = map(sub,q0,temp)
+			q0 = map(sub, q0, temp)
+	q0 = map(lambda  x : max(x, 0), q0)
 
+	# Add newly derived query words and reorder words in new query.
+	# Present the feedback summary according to precision rate in this iteration.
 	index = sorted(range(len(q0)),key=lambda i: q0[i])[-num-2:]
 	new_query = []
 	augmented_word = []
@@ -53,19 +58,16 @@ def expandWords (doc,relevant,query,prec,target_prec):
 		if word not in query_list:
 			augmented_word.append(word)
 
-	with open('transcript', 'a') as file:
+	with open('transcript.txt', 'a') as file:
 		print('=======================')
-		file.write('=======================\n')
 		print('FEEDBACK SUMMARY: ')
+		print('Query: ' + query)
+		print('Precision: ' + str(prec))
+		file.write('=======================\n')
 		file.write('FEEDBACK SUMMARY: \n')
-		if prec >= target_prec or prec == 0:
-			print('Query: '+query)
-			file.write('Query: '+query+'\n')
-		else:
-			print('Query: '+' '.join(new_query))
-			file.write('Query: '+' '.join(new_query)+'\n')
-		print('Precision: '+str(prec))
+		file.write('Query: ' + query + '\n')
 		file.write('Precision: '+str(prec)+'\n')
+
 		if prec < target_prec:
 			print('Still below the desired precision of '+str(target_prec))
 			file.write('Still below the desired precision of '+str(target_prec)+'\n')
@@ -73,7 +75,9 @@ def expandWords (doc,relevant,query,prec,target_prec):
 				print('Below desired precision, but can no longer augment the query')
 				file.write('Below desired precision, but can no longer augment the query'+'\n')
 			else:
+				print('Indexing results ....')
 				print('Augmenting by: '+' '.join(augmented_word))
+				file.wirte('Indexing results ....\n')
 				file.write('Augmenting by: '+' '.join(augmented_word)+'\n')
 		else:
 			print('Desired precision reached, done!')
@@ -82,8 +86,4 @@ def expandWords (doc,relevant,query,prec,target_prec):
 		file.write('=======================\n')
 
 	return ' '.join(new_query)
-
-
-
-
 
